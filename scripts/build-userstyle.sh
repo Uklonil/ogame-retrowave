b1de89adfd1d8603a54aa83508f4915f56c7ca4c
Francisco Javier Regaña Gil <giljamasev@gmail.com>
2026-08-21 23:48:22 +0200

    build: bundle research screen and bump userstyle version

--- scripts/build-userstyle.sh
+++ scripts/build-userstyle.sh
@@ -24,7 +24,7 @@ if not main.endswith('}'):
 main = main[:-1].rstrip()
 
 # Metadata for the distributable bundle.
-main = main.replace('@version        1.0.0', '@version        1.1.1')
+main = main.replace('@version        1.0.0', '@version        1.2.0')
 main = main.replace(
     '@updateURL      https://raw.githubusercontent.com/Uklonil/ogame-retrowave/main/main.css',
     '@updateURL      https://raw.githubusercontent.com/Uklonil/ogame-retrowave/main/ogame-retrowave.user.css'
@@ -34,6 +34,7 @@ parts = [
     'components/technology-grid.css',
     'screens/resources.css',
     'screens/facilities.css',
+    'screens/research.css',
 ]
 
 bundle = [main]
