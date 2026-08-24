7246c1a0e84ca5b49f265e7923864b46c6f9d72b
Francisco Javier Regaña Gil <giljamasev@gmail.com>
2026-08-25 00:48:15 +0200

    chore: bump develop userstyle to dev.10

--- scripts/build-develop-userstyle.sh
+++ scripts/build-develop-userstyle.sh
@@ -16,7 +16,7 @@ out = root / 'ogame-retrowave-develop.user.css.tmp'
 metadata = '''/* ==UserStyle==
 @name           OGame Retrowave — Develop
 @namespace      https://github.com/Uklonil/ogame-retrowave
-@version        2.0.0-dev.9
+@version        2.0.0-dev.10
 @description    Experimental full-theme rewrite for OGame 13. Develop channel; designed for Stylus and OGame Infinity.
 @author         Uklonil
 @homepageURL    https://github.com/Uklonil/ogame-retrowave/tree/develop
